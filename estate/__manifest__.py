e6a472d2e34b899f3ddbe04b04efdaa85ca7464a
Matt Purnell <matt.purnell98@gmail.com>
2024-09-30 10:09:13 +0200

    Add view xml

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,10 +1,11 @@
 {
     "name": "Estate",  # The name that will appear in the App list
-    "version": "17.0.0.6",  # Version
+    "version": "17.0.0.7",  # Version
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
     "data": [
         'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
     ],
     "installable": True,
     'license': 'LGPL-3',
