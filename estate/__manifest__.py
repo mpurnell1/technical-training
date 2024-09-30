97841bf21593f8d6f159acbb5db092f24b43ca68
Matt Purnell <matt.purnell98@gmail.com>
2024-09-30 09:59:12 +0200

    Add access rights

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,10 +1,10 @@
 {
     "name": "Estate",  # The name that will appear in the App list
-    "version": "17.0.0.4",  # Version
+    "version": "17.0.0.5",  # Version
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
     "data": [
-
+        'security/ir.model.access.csv',
     ],
     "installable": True,
     'license': 'LGPL-3',
