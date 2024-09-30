53e1477492e4c1432f0a70b9c046139d62eb445a
Matt Purnell <matt.purnell98@gmail.com>
2024-09-30 14:06:26 +0200

    Add buyer and salesperson

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -22,6 +22,8 @@ class EstateProperty(models.Model):
 
     # Relational fields
     property_type_id = fields.Many2one('estate.property.type', string="Property Type")
+    buyer_id = fields.Many2one('res.partner', string="Buyer", copy=False)
+    salesperson_id = fields.Many2one('res.users', string="Salesperson", default=lambda self: self.env.user)
 
     # Reserved fields
     active = fields.Boolean(default=True)
