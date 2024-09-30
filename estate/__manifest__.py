06fca0bf20d226c1d2766ac80e5e3683594277c1
Matt Purnell <matt.purnell98@gmail.com>
2024-09-30 13:28:36 +0200

    Reorder views

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,13 +1,13 @@
 {
     "name": "Estate",  # The name that will appear in the App list
-    "version": "17.0.0.21",  # Version
+    "version": "17.0.0.22",  # Version
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
     "data": [
         'security/ir.model.access.csv',
-        'views/estate_menus.xml',
         'views/estate_property_type_views.xml',
         'views/estate_property_views.xml',
+        'views/estate_menus.xml',
     ],
     "installable": True,
     'license': 'LGPL-3',
