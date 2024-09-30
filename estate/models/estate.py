831f529af149638d5d23cc2f43da3527a65c3343
Matt Purnell <matt.purnell98@gmail.com>
2024-09-30 09:49:59 +0200

    Add attributes to properties

--- estate/models/estate.py
+++ estate/models/estate.py
@@ -4,11 +4,11 @@ class EstateProperty(models.Model):
     _name = 'estate.property'
     _description = 'Real Estate Property'
 
-    name = fields.Char()
+    name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
     date_availability = fields.Date()
-    expected_price = fields.Float()
+    expected_price = fields.Float(required=True)
     selling_price = fields.Float()
     bedrooms = fields.Integer()
     living_area = fields.Integer()
@@ -16,9 +16,4 @@ class EstateProperty(models.Model):
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
-    garden_orientation = fields.Selection([
-        ('n', 'North'),
-        ('e', 'East'),
-        ('s', 'South'),
-        ('w', 'West'),
-    ])
+    garden_orientation = fields.Selection([('n', 'North'), ('e', 'East'), ('s', 'South'), ('w', 'West')])
