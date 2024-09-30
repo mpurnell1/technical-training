b462b55b309e84bb6dd5a413139f10d33e1868a2
Matt Purnell <matt.purnell98@gmail.com>
2024-09-30 14:22:11 +0200

    Fix copy-paste bug

--- estate/models/estate_property_tag.py
+++ estate/models/estate_property_tag.py
@@ -1,7 +1,7 @@
 from odoo import fields, models
 
 class EstatePropertyTag(models.Model):
-    _name = 'estate.property.type'
+    _name = 'estate.property.tag'
     _description = 'Real Estate Property Tag'
 
     name = fields.Char(required=True)
