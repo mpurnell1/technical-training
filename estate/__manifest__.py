7535afab6485d9f5555eea049af48df326dc83cb
Matt Purnell <matt.purnell98@gmail.com>
2024-09-30 13:10:40 +0200

    Add Estate Property Type model

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,11 +1,12 @@
 {
     "name": "Estate",  # The name that will appear in the App list
-    "version": "17.0.0.19",  # Version
+    "version": "17.0.0.20",  # Version
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
     "data": [
         'security/ir.model.access.csv',
         'views/estate_menus.xml',
+        'views/estate_property_type_views.xml',
         'views/estate_property_views.xml',
     ],
     "installable": True,
