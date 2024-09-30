7cbfda5f293e2aa83ddb7ac663ec61799645f4eb
Matt Purnell <matt.purnell98@gmail.com>
2024-09-30 10:00:58 +0200

    Remove unnecessary spaces

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Estate",  # The name that will appear in the App list
-    "version": "17.0.0.5",  # Version
+    "version": "17.0.0.6",  # Version
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
     "data": [
