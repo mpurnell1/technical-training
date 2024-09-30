24765b3d5ef5b1d20929e7db75952feb3eb9a128
Matt Purnell <matt.purnell98@gmail.com>
2024-09-30 10:27:07 +0200

    Add menus

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,10 +1,11 @@
 {
     "name": "Estate",  # The name that will appear in the App list
-    "version": "17.0.0.10",  # Version
+    "version": "17.0.0.11",  # Version
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
     "data": [
         'security/ir.model.access.csv',
+        'views/estate_menus.xml',
         'views/estate_property_views.xml',
     ],
     "installable": True,
