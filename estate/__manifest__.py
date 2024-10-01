e847208ea5814283f2eec69561324502e69bd603
Matt Purnell <matt.purnell98@gmail.com>
2024-10-01 11:11:21 +0200

    Minor bug fixes

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Estate",  # The name that will appear in the App list
-    "version": "18.0.0.23",  # Version
+    "version": "18.0.0.24",  # Version
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
     "data": [
--- estate/models/users.py
+++ estate/models/users.py
@@ -4,4 +4,4 @@ from odoo import fields, models
 class Users(models.Model):
     _inherit = 'res.users'
 
-    property_ids = fields.One2many('estate.property', 'salesperson_id', string="Properties", domain=['!', ('state', 'in', ('received', 'accepted'))])
+    property_ids = fields.One2many('estate.property', 'salesperson_id', string="Properties", domain=[('state', 'not in', ('accepted', 'sold'))])
