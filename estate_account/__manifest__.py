2dd8eb40826c46a1f706fef45c88a80a6b73928b
Matt Purnell <matt.purnell98@gmail.com>
2024-10-01 12:03:17 +0200

    Add invoice lines to created invoices

--- estate_account/__manifest__.py
+++ estate_account/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Estate Accounting",  # The name that will appear in the App list
-    "version": "18.0.0.1",  # Version
+    "version": "18.0.0.2",  # Version
     "application": False,  # This line says the module is a module, and not an App
     "depends": ["account", "estate"],  # dependencies
     "data": [
--- estate_account/models/property.py
+++ estate_account/models/property.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models, Command
 import logging
 
 _logger = logging.getLogger(__name__)
@@ -8,5 +8,20 @@ class Property(models.Model):
     _inherit = 'estate.property'
 
     def action_sold(self):
-        self.env['account.move'].create({'partner_id': self.buyer_id.id, 'move_type': 'out_invoice'})
+        self.env['account.move'].create({
+            'partner_id': self.buyer_id.id,
+            'move_type': 'out_invoice'
+            'invoice_line_ids': [
+                Command.create({
+                    'name': f'Commission for {self.name}',
+                    'quantity': 1,
+                    'price_unit': self.selling_price * 0.06,
+                }),
+                Command.create({
+                    'name': 'Administrative Fees',
+                    'quantity': 1,
+                    'price_unit': 100,
+                })
+            ]
+        })
         return super(Property, self).action_sold()
