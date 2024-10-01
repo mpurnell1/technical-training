b86899c7226879abf03467ec9e35a590bc22af50
Matt Purnell <matt.purnell98@gmail.com>
2024-10-01 12:48:45 +0200

    TUTORIAL COMPLETE - bump subversions

--- estate_account/__manifest__.py
+++ estate_account/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Estate Accounting",  # The name that will appear in the App list
-    "version": "18.0.0.3",  # Version
+    "version": "18.0.1.0",  # Version
     "application": False,  # This line says the module is a module, and not an App
     "depends": ["account", "estate"],  # dependencies
     "data": [
