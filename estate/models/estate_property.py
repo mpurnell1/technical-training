addc928d2faf7165f5bdb74b214288ada43f31d6
Matt Purnell <matt.purnell09@gmail.com>
2024-11-12 14:21:52 -0600

    Try to use filtered in unlink function

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -79,9 +79,8 @@ class EstateProperty(models.Model):
     # Object methods
     @api.ondelete(at_uninstall=False)
     def _unlink_if_new_or_cancelled(self):
-        for property in self:
-            if property.state not in ['new', 'canceled']:
-                raise UserError("You cannot delete a property that is not new or canceled.")
+        for _ in self.filtered([('state', 'not in', ['new', 'canceled'])]):
+            raise UserError("You cannot delete a property that is not new or canceled.")
 
     # Action methods
     def action_sold(self):
