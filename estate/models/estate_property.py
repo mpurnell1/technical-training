5188002e5a06f4a4807b03ec19929f85b95ee4b4
Matt Purnell <matt.purnell09@gmail.com>
2024-11-12 14:14:18 -0600

    Begin implementing suggestions made during review

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -3,10 +3,9 @@ from odoo.exceptions import UserError
 from odoo.tools.float_utils import float_compare, float_is_zero
 
 
-class Property(models.Model):
+class EstateProperty(models.Model):
     _name = 'estate.property'
     _description = 'Real Estate Property'
-    _order = 'id desc'
 
     # Custom fields
     name = fields.Char(string="Title", required=True)
@@ -27,25 +26,6 @@ class Property(models.Model):
     total_area = fields.Integer(string='Total Area (sqm)', compute='_compute_total_area')
     best_price = fields.Float(string='Best Offer', compute='_compute_best_price')
 
-    @api.depends('living_area', 'garden_area')
-    def _compute_total_area(self):
-        for record in self:
-            record.total_area = record.living_area + record.garden_area
-
-    @api.depends('offer_ids.price')
-    def _compute_best_price(self):
-        for record in self:
-            record.best_price = max(record.offer_ids.mapped('price') or [0])
-
-    @api.onchange('garden')
-    def _onchange_garden(self):
-        if not self.garden:
-            self.garden_area = 0
-            self.garden_orientation = False
-        else:
-            self.garden_area = 10
-            self.garden_orientation = 'n'
-
     # Relational fields
     property_type_id = fields.Many2one('estate.property.type', string="Property Type")
     buyer_id = fields.Many2one('res.partner', string="Buyer", copy=False)
@@ -59,13 +39,6 @@ class Property(models.Model):
         ('check_selling_price', 'CHECK(selling_price >= 0)', 'The selling price must be positive.'),
     ]
 
-    @api.constrains('selling_price', 'expected_price')
-    def _check_selling_price(self):
-        for record in self:
-            if not float_is_zero(record.selling_price, precision_digits=2) and \
-                float_compare(record.selling_price, (record.expected_price*9/10), precision_digits=2) == -1:
-                raise UserError("The selling price must be at least 90% of the expected price.")
-
     # Reserved fields
     active = fields.Boolean(default=True)
     state = fields.Selection(string="Status", default='new', copy=False, selection=[
@@ -75,24 +48,52 @@ class Property(models.Model):
         ('sold', 'Sold'),
         ('canceled', 'Canceled')])
 
+    # Compute methods
+    @api.depends('living_area', 'garden_area')
+    def _compute_total_area(self):
+        for property in self:
+            property.total_area = property.living_area + property.garden_area
+
+    @api.depends('offer_ids.price')
+    def _compute_best_price(self):
+        for property in self:
+            property.best_price = max(property.offer_ids.mapped('price') or [0])
+
+    @api.onchange('garden')
+    def _onchange_garden(self):
+        if not self.garden:
+            self.garden_area = 0
+            self.garden_orientation = False
+        else:
+            self.garden_area = 10
+            self.garden_orientation = 'n'
+
+    # Constraint methods
+    @api.constrains('selling_price', 'expected_price')
+    def _check_selling_price(self):
+        for property in self:
+            if not float_is_zero(property.selling_price, precision_digits=2) and \
+                float_compare(property.selling_price, (property.expected_price*9/10), precision_digits=2) == -1:
+                raise UserError("The selling price must be at least 90% of the expected price.")
+
     # Object methods
     @api.ondelete(at_uninstall=False)
     def _unlink_if_new_or_cancelled(self):
-        for record in self:
-            if record.state not in ['new', 'canceled']:
+        for property in self:
+            if property.state not in ['new', 'canceled']:
                 raise UserError("You cannot delete a property that is not new or canceled.")
 
     # Action methods
     def action_sold(self):
-        for record in self:
-            if record.state == 'canceled':
+        for property in self:
+            if property.state == 'canceled':
                 raise UserError("You cannot sell a canceled property.")
-            record.state = 'sold'
+            property.state = 'sold'
         return True
 
     def action_cancel(self):
-        for record in self:
-            if record.state == 'sold':
+        for property in self:
+            if property.state == 'sold':
                 raise UserError("You cannot cancel a sold property.")
-            record.state = 'canceled'
+            property.state = 'canceled'
         return True
